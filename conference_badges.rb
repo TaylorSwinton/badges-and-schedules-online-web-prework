ccf948eddd0eb429185e05784b1bbaa04168be25
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-12 00:02:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def assign_rooms(array)
   array.each_with_index do |name, index|
     newArray.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   end
-  
+
 end
 
 def printer(attendees)
